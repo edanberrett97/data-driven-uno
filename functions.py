5615b299fe20b33bbd9780383c91bb2206ccc8c6
edanberrett97 <edanberrett97@gmail.com>
2022-10-05 15:26:03 +0100

    general update

--- functions.py
+++ functions.py
@@ -1,3 +1,6 @@
+import numpy as np
+import pandas as pd
+
 #this function finds all cards in the list hand_cards that are allowed to be 
 #played if the top of the discard pile is discard_pile_card
 def eligible_cards(hand_cards,discard_pile_card):
@@ -20,7 +23,7 @@ def eligible_cards(hand_cards,discard_pile_card):
                     #card is allowed to be played
                     eligible.append(c)
             #checking if card is same type of action card as discard_pile_card
-            elif '_' in discard_pile_card:#str.split(discard_pile_card,'_')[-1] in ['skip','reverse']:
+            elif '_' in discard_pile_card:
                 if str.split(c,'_')[-1] == str.split(
                                                     discard_pile_card,'_')[-1]:
                     #card is allowed to be played
@@ -48,11 +51,115 @@ def card_score(card):
         return 50
     elif '_' in card:
         return 20
-    else:
+    elif len(card) == 2:
         return int(card[1])
+    else:
+        return 0
 
 #this function returns the number of points the cards held by a player are 
 #worth in total
 def hand_score(hand_cards):
     return sum([card_score(c) for c in hand_cards])
+
+#returns all normal cards from full_deck (with all cards from hand_cards removed) 
+#where if that card was on the top the deck, at least one of the card from a
+#player's hand hand_cards would be playable; a rough proxy for the probability
+#that the player will be able to play one of their cards when it is next their
+#turn 
+def hand_cards_normal_domain(hand_cards,full_deck):
+    full_deck_normal_cards = [dc for dc in full_deck if len(dc) == 2]
+    domain_unique = []
+    for hc in hand_cards:
+        if 'wild' in hc:
+            domain_unique += [dc for dc in full_deck_normal_cards]
+        elif '_' in hc:
+            domain_unique += [dc for dc in full_deck_normal_cards if 
+                                                              dc[0] == hc[0]]
+        elif hc != '':
+            domain_unique += [dc for dc in full_deck_normal_cards if 
+                                              dc[0] == hc[0] or dc[1] == hc[1]]
+    domain_unique = set(domain_unique)
+    domain = [c for c in full_deck if c in domain_unique]
+    for c in hand_cards:
+        if len(c) == 2:
+            domain.remove(c)
+    return domain
+    
+#
+def count_hand_cards_of_type(hand_cards,type):
+    if type == 'number':
+        return len([c for c in hand_cards if len(c) == 2])
+    if type == 'wild':
+        return len([c for c in hand_cards if 'wild' in c and '+4' not in c])
+    else:
+        return len([c for c in hand_cards if type in c])
+    
+#
+def count_distinct_numbers_or_actions_in_hand(hand_cards):
+    number_cards = [c for c in hand_cards if len(c) == 2]
+    action_cards = [c for c in hand_cards if len(str.split(c,'_')) == 2]
+    unique_numbers = set([c[-1] for c in number_cards])
+    unique_actions = set([str.split(c,'_')[-1] for c in action_cards])
+    n_unique_numbers_or_actions = len(unique_numbers) + len(unique_actions)
+    return n_unique_numbers_or_actions
+
+#
+def count_distinct_colours_in_hand(hand_cards):
+    non_wild_cards = [c for c in hand_cards if 'wild' not in c]
+    unique_colours = set([c[0] for c in non_wild_cards])
+    return len(unique_colours)
+
+#
+def card_type(card):
+    if card != '':
+        if len(card) == 2 and card[-1] not in ['0','7']:
+            return 'number'
+        if '_' in card:
+            return str.split(card,'_')[-1]
+        if card[-1] in ['0','7']:
+            return card[-1]
+        if 'wild+4' in card:
+            return 'wild+4'
+        else:
+            return 'wild'
+    else:
+        return 'None'
+    
+#
+def delta(x,y):
+    if x == y:
+        return 1
+    else:
+        return 0
+    
+#
+def card_type_is_type(card,type):
+    return delta(card_type(card),type)
+
+#
+def m_players_later(direction_pre_play,player,N,m):
+    return (player + m * direction_pre_play) % N
+
+#returns number of cards held by the player whose turn it will be m turns later
+#if the direction of play doesn't change e.g. with 5 players, player 2 as 
+#current player and direction being anti-clockwise, 1 turn later will be player
+#1, 2 turns later will be player 5
+def m_players_later_n_cards(direction_pre_play,player,player_hands,m):
+    m_players_later_player = (player + m * direction_pre_play) % len(
+                                                                player_hands) 
+    #print(m_players_later_player,'a')
+    if m_players_later_player == 0:
+        m_players_later_player += len(player_hands)
+    #print(m_players_later_player,'b')
+    return len(player_hands['player_' + str(m_players_later_player)])
+
+#
+def m_players_later_score(direction_pre_play,player,player_scores,m):
+    m_players_later_player = (player + m * direction_pre_play) % len(
+                                                                player_scores) 
+    #print(m_players_later_player,'a')
+    if m_players_later_player == 0:
+        m_players_later_player += len(player_scores)
+    #print(m_players_later_player,'b')
+    return player_scores['player_' + str(m_players_later_player)]
 
